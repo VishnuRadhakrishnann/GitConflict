62d1703fe20693833b36b9a93fcd22a47ef8461d
VishnuRadhakrishnann <vishnu31595@gmail.com>
2020-07-20 20:55:43 +0530

    greens

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,14 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOmr() {
+		System.out.println("It is a main branch");
+		 }
+	public void greensAdayar() {
+		System.out.println("This is one of sub branch");
+	}
+
+		
+		
+	}
+
